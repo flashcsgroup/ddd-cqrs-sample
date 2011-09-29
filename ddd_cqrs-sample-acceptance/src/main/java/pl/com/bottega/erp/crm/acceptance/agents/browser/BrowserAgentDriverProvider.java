d60278f38d44443f3178672b3bc5d8479e48a469
rafaljamroz@gmail.com <rafaljamroz@gmail.com@5cbc8567-314e-da0a-5484-a3f56dc8e627>
2011-09-29 15:43:20 +0000

    acceptance tests - enabling to run Chromium from a specific location

--- ddd_cqrs-sample-acceptance/src/main/java/pl/com/bottega/erp/crm/acceptance/agents/browser/BrowserAgentDriverProvider.java
+++ ddd_cqrs-sample-acceptance/src/main/java/pl/com/bottega/erp/crm/acceptance/agents/browser/BrowserAgentDriverProvider.java
@@ -1,22 +1,34 @@
 package pl.com.bottega.erp.crm.acceptance.agents.browser;
 
+import org.apache.commons.lang.StringUtils;
 import org.jbehave.core.annotations.AfterStories;
 import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.BeforeStories;
 import org.jbehave.web.selenium.PropertyWebDriverProvider;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import pl.com.bottega.erp.crm.acceptance.spring.BrowserAgent;
 
 @BrowserAgent
-public class FlakyIePropertyWebDriverProvider extends PropertyWebDriverProvider {
+public class BrowserAgentDriverProvider extends PropertyWebDriverProvider {
 
     @Override
     protected InternetExplorerDriver createInternetExplorerDriver() {
-        DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
-        ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-        return new InternetExplorerDriver(ieCapabilities);
+        DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+        capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        return new InternetExplorerDriver(capabilities);
+    }
+
+    @Override
+    protected ChromeDriver createChromeDriver() {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        String chromeBinary = System.getProperty("chrome.binary");
+        if (StringUtils.isNotBlank(chromeBinary)) {
+            capabilities.setCapability("chrome.binary", chromeBinary);
+        }
+        return new ChromeDriver(capabilities);
     }
 
     @BeforeStories
