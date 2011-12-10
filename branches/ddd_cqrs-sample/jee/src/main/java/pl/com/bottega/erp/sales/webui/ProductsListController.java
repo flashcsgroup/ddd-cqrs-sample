2be18604b1dc73f79ddf69f2e3121b25556f3618
panciesielski@gmail.com <panciesielski@gmail.com@5cbc8567-314e-da0a-5484-a3f56dc8e627>
2011-12-10 15:59:02 +0000

    JEE: Change of persistence provider; First JSF page prototype

--- branches/ddd_cqrs-sample/jee/src/main/java/pl/com/bottega/erp/sales/webui/ProductsListController.java
+++ branches/ddd_cqrs-sample/jee/src/main/java/pl/com/bottega/erp/sales/webui/ProductsListController.java
@@ -1,5 +1,42 @@
 package pl.com.bottega.erp.sales.webui;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import pl.com.bottega.erp.sales.presentation.ProductFinder;
+import pl.com.bottega.erp.sales.presentation.ProductListItemDto;
+import pl.com.bottega.erp.sales.presentation.ProductSearchCriteria;
+
+@Named(value="products")
 public class ProductsListController {
+	
+	@Inject
+	ProductFinder finder;
+	ProductSearchCriteria searchCriteria = new ProductSearchCriteria();
+	
+	private List<ProductListItemDto> list = new ArrayList<ProductListItemDto>();
+	
+	public List<ProductListItemDto> getItems()
+	{
+		list =  finder.findProducts(searchCriteria).getItems();
+		return list;
+	}
+	
+	public int getTotalItemsCount()
+	{
+		return 1;
+	}
+	
+	public void sortByName()
+	{
+		
+	}
+	
+	public void sortByPrice()
+	{
+		
+	}
 }
