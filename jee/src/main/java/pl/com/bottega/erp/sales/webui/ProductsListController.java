1b7b5721b507cd5dd573ce232c0a78fc95e90ab3
panciesielski@gmail.com <panciesielski@gmail.com@5cbc8567-314e-da0a-5484-a3f56dc8e627>
2011-12-11 00:04:00 +0000

    JEE: Some page fixes and necessary changes to directory structures

--- jee/src/main/java/pl/com/bottega/erp/sales/webui/ProductsListController.java
+++ jee/src/main/java/pl/com/bottega/erp/sales/webui/ProductsListController.java
@@ -1,14 +1,15 @@
 package pl.com.bottega.erp.sales.webui;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import pl.com.bottega.cqrs.query.PaginatedResult;
 import pl.com.bottega.erp.sales.presentation.ProductFinder;
 import pl.com.bottega.erp.sales.presentation.ProductListItemDto;
 import pl.com.bottega.erp.sales.presentation.ProductSearchCriteria;
+import pl.com.bottega.erp.sales.presentation.ProductSearchCriteria.ProductSearchOrder;
 
 @Named(value="products")
 public class ProductsListController {
@@ -17,25 +18,80 @@ public class ProductsListController {
 	ProductFinder finder;
 	ProductSearchCriteria searchCriteria = new ProductSearchCriteria();
 	
-	private List<ProductListItemDto> list = new ArrayList<ProductListItemDto>();
+	private PaginatedResult<ProductListItemDto> finderResult;
+	
+	private static final int RESULTS_PER_PAGE = 10;
+	
+	
 	
 	public List<ProductListItemDto> getItems()
 	{
-		list =  finder.findProducts(searchCriteria).getItems();
-		return list;
+		if (finderResult == null)
+		{
+			fetch();
+		}
+		return finderResult.getItems();
 	}
 	
 	public int getTotalItemsCount()
 	{
-		return 1;
+		if (finderResult == null)
+		{
+			fetch();
+		}
+
+		return finderResult.getTotalItemsCount();
 	}
 	
 	public void sortByName()
 	{
 		
+		searchCriteria.setOrderBy(ProductSearchOrder.NAME);
 	}
 	
 	public void sortByPrice()
+	{
+		searchCriteria.setOrderBy(ProductSearchOrder.PRICE);
+	}
+	
+	private void fetch()
+	{
+		finderResult =  finder.findProducts(searchCriteria);
+	}
+	
+	public int getPagesCount()
+	{
+		if (finderResult == null)
+		{
+			fetch();
+		}
+		
+		return finderResult.getPagesCount();
+	}
+	
+	public String getContainsTextFilter()
+	{
+		return searchCriteria.getContainsText();
+	}
+	
+	public void setContainsTextFilter(String containsText)
+	{
+	    if (containsText != null) {
+	    	searchCriteria.setContainsText(containsText.trim());
+        }
+	}
+	
+	public void setMaxPriceFilter(Double d)
+	{
+		searchCriteria.setMaxPrice(d);
+	}
+	
+	public Double getMaxPriceFilter()
+	{
+		return searchCriteria.getMaxPrice();
+	}
+	
+	public void addToOrder(Long productId)
 	{
 		
 	}
