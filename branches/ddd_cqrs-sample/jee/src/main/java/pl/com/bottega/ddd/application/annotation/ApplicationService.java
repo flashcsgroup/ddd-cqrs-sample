4b3f0f2f4876b2f3a4462c783f20653d02052374
ssobot@gmail.com <ssobot@gmail.com@5cbc8567-314e-da0a-5484-a3f56dc8e627>
2011-12-12 21:12:07 +0000

    bugfix: rentention added to the annotation in order to by able to know transaction detail in runtime

--- branches/ddd_cqrs-sample/jee/src/main/java/pl/com/bottega/ddd/application/annotation/ApplicationService.java
+++ branches/ddd_cqrs-sample/jee/src/main/java/pl/com/bottega/ddd/application/annotation/ApplicationService.java
@@ -3,12 +3,19 @@
  */
 package pl.com.bottega.ddd.application.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 
 /**
  * @author Slawek
  * 
  */
 //@Secured
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ApplicationService {
     String value() default "";
 }
