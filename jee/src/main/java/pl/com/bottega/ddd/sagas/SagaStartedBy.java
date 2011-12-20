bc683681321b4dfa2e5f3fa957dfc62139779b16
panciesielski@gmail.com <panciesielski@gmail.com@5cbc8567-314e-da0a-5484-a3f56dc8e627>
2011-12-20 00:07:37 +0000

    Finished paging and filtering on JSF product list

--- jee/src/main/java/pl/com/bottega/ddd/sagas/SagaStartedBy.java
+++
@@ -1,6 +0,0 @@
-package pl.com.bottega.ddd.sagas;
-
-public interface SagaStartedBy<T> {
-
-	public void handle(T event);
-}
--- jee/src/main/java/pl/com/bottega/erp/sales/webui/ProductsListController.java
+++ jee/src/main/java/pl/com/bottega/erp/sales/webui/ProductsListController.java
@@ -2,8 +2,12 @@ package pl.com.bottega.erp.sales.webui;
 
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import javax.inject.Named;
+
+import com.sun.mail.imap.protocol.SearchSequence;
 
 import pl.com.bottega.cqrs.query.PaginatedResult;
 import pl.com.bottega.erp.sales.presentation.ProductFinder;
@@ -11,28 +15,56 @@ import pl.com.bottega.erp.sales.presentation.ProductListItemDto;
 import pl.com.bottega.erp.sales.presentation.ProductSearchCriteria;
 import pl.com.bottega.erp.sales.presentation.ProductSearchCriteria.ProductSearchOrder;
 
-@Named(value="products")
+@ManagedBean(name="products")
+@SessionScoped
 public class ProductsListController {
-	
+
 	@Inject
-	ProductFinder finder;
-	ProductSearchCriteria searchCriteria = new ProductSearchCriteria();
-	
+	private ProductFinder finder;
+
+	private ProductSearchCriteria searchCriteria = new ProductSearchCriteria();
+
 	private PaginatedResult<ProductListItemDto> finderResult;
-	
+
 	private static final int RESULTS_PER_PAGE = 10;
-	
-	
-	
-	public List<ProductListItemDto> getItems()
+	private static final int START_PAGE = 1;
+
+	private int pageNumber()
 	{
-		if (finderResult == null)
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		String param = (String) facesContext.getExternalContext().getRequestParameterMap().get("page");
+		if (param == null)
 		{
-			fetch();
+			return START_PAGE;
 		}
-		return finderResult.getItems();
+		try
+		{
+			Integer intParam = Integer.parseInt(param);
+			return intParam;
+		}
+		catch (NumberFormatException e)
+		{
+			return START_PAGE;
+		}
+
+	}
+
+	public boolean isAscending()
+	{
+		return searchCriteria.isAscending();
 	}
 	
+	public ProductSearchOrder getOrderBy()
+	{
+		return searchCriteria.getOrderBy();
+	}
+	public List<ProductListItemDto> getItems()
+	{
+		fetch();
+		return finderResult.getItems();
+	}
+
 	public int getTotalItemsCount()
 	{
 		if (finderResult == null)
@@ -42,57 +74,79 @@ public class ProductsListController {
 
 		return finderResult.getTotalItemsCount();
 	}
-	
+
 	public void sortByName()
 	{
-		
+		if (ProductSearchOrder.NAME.equals(searchCriteria.getOrderBy()))
+		{
+			searchCriteria.setAscending(!searchCriteria.isAscending());
+
+		}
 		searchCriteria.setOrderBy(ProductSearchOrder.NAME);
 	}
-	
+
 	public void sortByPrice()
 	{
+		if (ProductSearchOrder.PRICE.equals(searchCriteria.getOrderBy()))
+		{
+			searchCriteria.setAscending(!searchCriteria.isAscending());
+		}
 		searchCriteria.setOrderBy(ProductSearchOrder.PRICE);
 	}
-	
+
 	private void fetch()
 	{
+		searchCriteria.setPageNumber(pageNumber());
+		searchCriteria.setItemsPerPage(RESULTS_PER_PAGE);
 		finderResult =  finder.findProducts(searchCriteria);
 	}
-	
+
 	public int getPagesCount()
 	{
 		if (finderResult == null)
 		{
 			fetch();
 		}
-		
+
 		return finderResult.getPagesCount();
 	}
-	
+
 	public String getContainsTextFilter()
 	{
 		return searchCriteria.getContainsText();
 	}
-	
+
 	public void setContainsTextFilter(String containsText)
 	{
-	    if (containsText != null) {
-	    	searchCriteria.setContainsText(containsText.trim());
-        }
+		if (containsText != null) {
+			searchCriteria.setContainsText(containsText.trim());
+		}
 	}
-	
+
 	public void setMaxPriceFilter(Double d)
 	{
 		searchCriteria.setMaxPrice(d);
 	}
-	
+
 	public Double getMaxPriceFilter()
 	{
 		return searchCriteria.getMaxPrice();
 	}
-	
+
 	public void addToOrder(Long productId)
 	{
-		
+
+	}
+
+	public String doFilter()
+	{
+		return null;
+	}
+
+	public String clearFilter()
+	{
+		searchCriteria = new ProductSearchCriteria();
+		finderResult = null;
+		return null;
 	}
 }
